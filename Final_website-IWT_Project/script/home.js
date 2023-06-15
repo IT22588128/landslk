267e193ba6fa7ca25b5dbb800e1f994a48d2a56e
IT22588128 <127617127+IT22588128@users.noreply.github.com>
2023-06-15 17:14:04 +0530

    Final_website-IWT_Project
    Final website

---
+++ Final_website-IWT_Project/script/home.js
@@ -0,0 +1,50 @@
+window.addEventListener('DOMContentLoaded', function () {
+    var slides = document.querySelectorAll('.slide');
+    var currentSlide = 0;
+    var totalSlides = slides.length;
+    var slideInterval = setInterval(nextSlide,5000);
+  
+    function nextSlide() {
+      slides[currentSlide].classList.remove('active');
+      currentSlide = (currentSlide + 1) % totalSlides;
+      slides[currentSlide].classList.add('active');
+    }
+  });
+
+
+
+
+
+
+
+
+
+  function search_land() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('l1');
+      
+    selectElement = document.querySelector('#city');
+    output = selectElement.options[selectElement.selectedIndex].value;
+
+    for (i = 0; i < x.length; i++) {
+
+        if (!x[i].innerHTML.toLowerCase().includes(input) || !x[i].innerHTML.toLowerCase().includes(output)) {
+          x[i].style.position="absolute";
+          x[i].style.opacity=0;
+        }
+        else {
+          x[i].style.position="relative";
+            x[i].style.opacity=1;
+                 
+        }
+
+    }
+}
+
+
+
+
+
+
+
